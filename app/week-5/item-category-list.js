0f656f6c7b89e2454092c1e405ce2b18bb673139
winko <winkopeng@gmail.com>
2024-02-21 16:05:31 -0700

    week5

---
+++ app/week-5/item-category-list.js
@@ -0,0 +1,18 @@
+import Item from "./item.js";
+
+export default function ItemCategory({ category, items }) {    
+    items = items.filter((item) => item.category === category);
+    return (
+        <>
+            <h3 className="capitalize text-xl">{category}</h3>
+            {items.map((item) => (               
+                <Item 
+                name={item.name}
+                quantity={item.quantity}
+                category={item.category} 
+                key={item.id} 
+                />               
+            ))} 
+        </>         
+    );    
+}
---
+++ app/week-5/page.js
@@ -0,0 +1,13 @@
+import ItemList from './item-list.js'
+
+export default function Page() {
+    return (
+      <main className="bg-slate-950">
+        <div className="m-4"><h2 className="text-3xl font-bold m-2">Shopping List</h2>
+            <ul>
+                <ItemList />
+            </ul>
+        </div>
+      </main>
+    );
+  }
